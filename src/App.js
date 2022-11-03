7f5dbcbe5b1688411aeae3f75bb4503fd39a1e25
changhak0310 <dlckdgkr0320@naver.com>
2022-11-03 12:14:04 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/users/Home";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+const theme = createTheme({
+  palette: {
+     primary: {
+       light: '#757ce8',
+       main: '#3f50b5',
+       dark: '#002884',
+       contrastText: '#fff',
+     },
+     secondary: {
+       light: '#ff7961',
+       main: '#f44336',
+       dark: '#ba000d',
+       contrastText: '#000',
+     },
+   }
+ })
+ 
 function App() {
   return (
-    <Routes>
-      <Route path="/admin">
-        <Route index path="home" element={<Home/>}/>
-      </Route>
-      <Route path="/users">
-        <Route index path="home" element={<Home/>}/>
-      </Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/admin">
+          <Route index path="home" element={<Home/>}/>
+        </Route>
+        <Route path="/users">
+          <Route index path="home" element={<Home/>}/>
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CssBaseline/>
     <BrowserRouter>
       <App />
     </BrowserRouter>
